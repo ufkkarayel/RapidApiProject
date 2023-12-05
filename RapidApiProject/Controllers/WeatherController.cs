1204a26a4b75e84aa3fc6e9364f9dc20b685132f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-05 22:23:57 +0300

    hava durumu apisi eklendi

---
+++ RapidApiProject/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace RapidApiProject.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://yahoo-weather5.p.rapidapi.com/weather?location=ankara&format=json&u=c"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ed120ca162msh4a2cf1663625a24p1cae18jsn548260e0d857" },
+        { "X-RapidAPI-Host", "yahoo-weather5.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+               
+            }
+            return View();
+        }
+    }
+}
---
+++ RapidApiProject/Models/WeatherViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RapidApiProject.Models
+{
+    public class WeatherViewModel
+    {
+        public string city { get; set; }
+        public string country { get; set; }
+        public string timezone_id { get; set; }
+        public decimal lat { get; set; }
+        //public decimal lon { get; set; }
+
+    }
+}
