9e0a2bd477eef809be88340dd01eb09bddeaf17a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-05 21:56:58 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/MovieSeriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApiProject.Controllers
+{
+    public class MovieSeriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ed120ca162msh4a2cf1663625a24p1cae18jsn548260e0d857" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<SeriesViewModel>>(body);
+                return View(values);
+            }
+            
+        }
+    }
+}
+
